22ae477efbec5e06cd1f20402c5a684ead8d270c
ktkt200612 <ktkt200612@gmail.com>
2022-05-24 21:06:30 +0900

    maigration

---
+++ database/migrations/2022_05_23_222726_create_clients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->varchar('tel');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->integer('status_id');
+            $table->integer('user_id');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+    //      $table->foreign('status_id')->references('id')->on('statuses');
+    //      $table->foreign('user_id')->references('id')->on('users');
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('clients');
+    }
+}
--- database/migrations/2022_05_23_225540_create_statuses_table.php
+++ database/migrations/2022_05_23_225540_create_statuses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSlacksTable extends Migration
+class CreateStatusesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,10 @@ class CreateSlacksTable extends Migration
      */
     public function up()
     {
-        Schema::create('slacks', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateSlacksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('slacks');
+        Schema::dropIfExists('statuses');
     }
 }
